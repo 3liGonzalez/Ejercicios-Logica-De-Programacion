373dd315df6a2c25f12d528d558f760cf5199b94
eli <eli.hispanicas.unam@gmail.com>
2024-02-23 02:26:57 -0600

    Ejercicios-Logica-De-Programacion diagramas guardar

--- Ejercicio de Lógica de Programación 1/numeros.js
+++ Ejercicio de Lógica de Programación 1/numeros.js
@@ -12,10 +12,10 @@ var minNum = Math.min(n1, n2, n3);
 var middleNum = (n1 + n2 + n3) - maxNum - minNum;
 
 // Imprimir los números ordenados de mayor a menor
-console.log( maxNum + middleNum + minNum);
+console.log(maxNum + middleNum + minNum);
 
 // Imprimir los números ordenados de menor a mayor
-console.log( minNum + middleNum + maxNum);
+console.log(minNum + middleNum + maxNum);
 
 // Verificar si los números son iguales e imprimir un mensaje correspondiente
 if (n1 === n2 && n2 === n3) {
